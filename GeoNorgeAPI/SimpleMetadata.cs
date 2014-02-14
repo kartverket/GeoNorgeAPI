e9b4911feed431a3ef8665ad58179759f5c9e566
Henning Jensen <henning@arkitektum.no>
2014-02-14 16:21:33 +0100

    Reading and updating contact fields.

--- GeoNorgeAPI/SimpleMetadata.cs
+++ GeoNorgeAPI/SimpleMetadata.cs
@@ -187,5 +187,178 @@ namespace GeoNorgeAPI
                 }
             }
         }
+
+        public SimpleContact ContactPublisher
+        {
+            get { return GetContactWithRole("publisher"); }
+            set { CreatOrUpdateContactWithRole("publisher", value); }
+        }
+
+        public SimpleContact ContactPointOfContact
+        {
+            get { return GetContactWithRole("pointOfContact"); }
+            set { CreatOrUpdateContactWithRole("pointOfContact", value); }
+        }
+
+        private void CreatOrUpdateContactWithRole(string roleCodeValue, SimpleContact contact)
+        {
+            CI_ResponsibleParty_Type responsibleParty = GetContactInformationResponsiblePartyWithRole(roleCodeValue);
+
+            if (responsibleParty == null)
+            {
+                responsibleParty = new CI_ResponsibleParty_Type();
+                
+                var newPointOfContactArray = new CI_ResponsibleParty_PropertyType[] {
+                        new CI_ResponsibleParty_PropertyType {
+                            CI_ResponsibleParty = responsibleParty
+                        }                        
+                    };
+
+                var identification = GetIdentificationNotNull();
+                if (identification.pointOfContact == null)
+                {
+                    identification.pointOfContact = newPointOfContactArray;
+                }
+                else
+                {
+                    identification.pointOfContact = identification.pointOfContact.Concat(newPointOfContactArray).ToArray();
+                }
+            }
+
+            responsibleParty.individualName = new CharacterString_PropertyType { CharacterString = contact.Name };
+            responsibleParty.organisationName = new CharacterString_PropertyType { CharacterString = contact.Organization };
+            if (responsibleParty.contactInfo == null)
+            {
+                responsibleParty.contactInfo = new CI_Contact_PropertyType
+                {
+                    CI_Contact = new CI_Contact_Type
+                    {
+                        address = new CI_Address_PropertyType
+                        {
+                            CI_Address = new CI_Address_Type()
+                        }
+                    }
+                };
+            }
+            if (responsibleParty.contactInfo.CI_Contact == null)
+            {
+                responsibleParty.contactInfo.CI_Contact = new CI_Contact_Type
+                    {
+                        address = new CI_Address_PropertyType
+                        {
+                            CI_Address = new CI_Address_Type()
+                        }
+                    };
+            }
+            if (responsibleParty.contactInfo.CI_Contact.address == null)
+            {
+                responsibleParty.contactInfo.CI_Contact.address = new CI_Address_PropertyType
+                {
+                    CI_Address = new CI_Address_Type()
+                };
+            }
+            if (responsibleParty.contactInfo.CI_Contact.address.CI_Address == null)
+            {
+                responsibleParty.contactInfo.CI_Contact.address.CI_Address = new CI_Address_Type();
+            }
+
+
+            responsibleParty.contactInfo.CI_Contact.address.CI_Address.electronicMailAddress = new CharacterString_PropertyType[] { 
+                    new CharacterString_PropertyType { CharacterString = contact.Email }
+                };
+
+            responsibleParty.role = new CI_RoleCode_PropertyType
+            {
+                CI_RoleCode = new CodeListValue_Type
+                {
+                    codeList = "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/Codelist/ML_gmxCodelists.xml#CI_RoleCode",
+                    codeListValue = contact.Role
+                }
+            };
+        }
+
+        private CI_ResponsibleParty_Type GetContactInformationResponsiblePartyWithRole(string roleCodeValue) 
+        {
+            CI_ResponsibleParty_Type contact = null;
+            var identification = GetIdentification();
+            if (identification != null && identification.pointOfContact != null)
+            {
+                foreach (var responsibleParty in identification.pointOfContact)
+                {
+                    if (responsibleParty.CI_ResponsibleParty != null)
+                    {
+                        if (responsibleParty.CI_ResponsibleParty.role != null && responsibleParty.CI_ResponsibleParty.role.CI_RoleCode != null
+                            && responsibleParty.CI_ResponsibleParty.role.CI_RoleCode.codeListValue != null
+                            && responsibleParty.CI_ResponsibleParty.role.CI_RoleCode.codeListValue.ToLower() == roleCodeValue.ToLower())
+                        {
+                            contact = responsibleParty.CI_ResponsibleParty;
+                            break;
+                        }
+                    }
+                }
+            }
+            return contact;
+        }
+
+        private SimpleContact GetContactWithRole(string roleCodeValue)
+        {
+            SimpleContact contact = null;
+
+            var identification = GetIdentification();
+            if (identification != null && identification.pointOfContact != null)
+            {
+                foreach (var responsibleParty in identification.pointOfContact)
+                {
+                    if (responsibleParty.CI_ResponsibleParty != null)
+                    {
+                        if (responsibleParty.CI_ResponsibleParty.role != null && responsibleParty.CI_ResponsibleParty.role.CI_RoleCode != null
+                            && responsibleParty.CI_ResponsibleParty.role.CI_RoleCode.codeListValue != null
+                            && responsibleParty.CI_ResponsibleParty.role.CI_RoleCode.codeListValue.ToLower() == roleCodeValue.ToLower())
+                        {
+                            var p = responsibleParty.CI_ResponsibleParty;
+
+                            string email = null;
+                            if (p.contactInfo != null && p.contactInfo.CI_Contact != null && p.contactInfo.CI_Contact.address != null 
+                                && p.contactInfo.CI_Contact.address.CI_Address != null
+                                && p.contactInfo.CI_Contact.address.CI_Address.electronicMailAddress != null
+                                && p.contactInfo.CI_Contact.address.CI_Address.electronicMailAddress[0] != null
+                                && p.contactInfo.CI_Contact.address.CI_Address.electronicMailAddress[0].CharacterString != null)
+                            {
+                                email = p.contactInfo.CI_Contact.address.CI_Address.electronicMailAddress[0].CharacterString;
+                            }
+
+                            string role = null;
+                            if (p.role != null && p.role.CI_RoleCode != null)
+                            {
+                                role = p.role.CI_RoleCode.codeListValue;
+                            }
+
+                            contact = new SimpleContact
+                            {
+                                Name = GetStringOrNull(p.individualName),
+                                Organization = GetStringOrNull(p.organisationName),
+                                Email = email,
+                                Role = role
+                            };
+                            break;
+                        }
+                    }
+                }
+            }
+            return contact;
+        }
+
+        private string GetStringOrNull(CharacterString_PropertyType input)
+        {
+            return input != null ? input.CharacterString : null;
+        }
+    }
+
+    public class SimpleContact
+    {
+        public string Name { get; set; }
+        public string Organization { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
     }
 }
