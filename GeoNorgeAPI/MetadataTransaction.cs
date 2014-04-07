c16b1879b7d626f7026da4b900aaff2fc56ade72
Henning Jensen <henning@arkitektum.no>
2014-04-07 10:03:17 +0200

    Add missing object MetadataTransaction.

---
+++ GeoNorgeAPI/MetadataTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoNorgeAPI
+{
+    public class MetadataTransaction
+    {
+        public string TotalInserted { get; set; }
+        public string TotalUpdated { get; set; }
+        public string TotalDeleted { get; set; }
+
+        public List<string> Identifiers { get; set; }
+    }
+}
