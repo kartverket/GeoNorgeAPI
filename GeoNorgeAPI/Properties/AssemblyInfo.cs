fc8b9f1f3629cfe3d7586a49ad9bae45af0b2701
Henning Jensen <henning@arkitektum.no>
2014-11-17 09:33:26 +0100

    Version 2.6.7

--- GeoNorgeAPI/Properties/AssemblyInfo.cs
+++ GeoNorgeAPI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arkitektum AS")]
 [assembly: AssemblyProduct("GeoNorgeAPI")]
-[assembly: AssemblyCopyright("Copyright © Kartverket 2013")]
+[assembly: AssemblyCopyright("Copyright © Kartverket 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.6")]
+[assembly: AssemblyVersion("2.6.7")]
