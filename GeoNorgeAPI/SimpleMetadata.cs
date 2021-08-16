16def1648fc5cfd1a63e78bc6037de7868346438
Dag Olav Dahle <dagolav@arkitektum.no>
2021-08-16 13:01:34 +0200

    Add Contact PositionName

--- GeoNorgeAPI/SimpleMetadata.cs
+++ GeoNorgeAPI/SimpleMetadata.cs
@@ -641,7 +641,10 @@ namespace GeoNorgeAPI
             {
                 responsibleParty.organisationName = new CharacterString_PropertyType { CharacterString = contact.Organization };
             }
-            
+
+            if (!string.IsNullOrEmpty(contact.PositionName))
+                responsibleParty.positionName = new CharacterString_PropertyType { CharacterString = contact.PositionName };
+
             if (responsibleParty.contactInfo == null)
             {
                 responsibleParty.contactInfo = new CI_Contact_PropertyType
@@ -716,7 +719,8 @@ namespace GeoNorgeAPI
                 Organization = GetStringOrNull(responsibleParty.organisationName),
                 OrganizationEnglish = GetEnglishValueFromFreeText(responsibleParty.organisationName),
                 Email = email,
-                Role = role
+                Role = role,
+                PositionName = GetStringOrNull(responsibleParty.positionName)
             };
         }
 
@@ -4318,6 +4322,7 @@ namespace GeoNorgeAPI
         public string OrganizationEnglish { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
+        public string PositionName { get; set; }
     }
 
     public class SimpleKeyword
